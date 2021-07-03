6b678114ac2496619cd308b1265c2c4dc9f2fe25
Hoang Quy <vdr_quyhoang@airasia.com>
2021-07-03 22:59:56 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,5 @@
-import 'styles/globals.scss';
-import classes from './app.module.scss';
-const APP_NAME = 'Right Person, Right Job | Job4U';
-
 const MyApp = ({ Component, pageProps }) => {
-    return (
-        <>
-            <div className={classes.layoutWrapper}>
-                <div>{APP_NAME}</div>
-                <div className={classes.layoutMain}>
-                    <Component {...pageProps} />
-                </div>
-            </div>
-        </>
-    );
+    return <Component {...pageProps} />
 };
 
 export default MyApp;
