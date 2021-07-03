783bc48dbad0886e27e9f976a12758658e0c3e1f
Hoang Quy <vdr_quyhoang@airasia.com>
2021-07-03 22:52:32 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <div style={{ padding: '24px', textAlign: 'center' }}>Container</div>;
+  return (
+    <div style={{ padding: '24px', textAlign: 'center' }}>
+      Container 1
+    </div>
+  );
 };
 
 export default Home;
